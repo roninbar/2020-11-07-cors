785d43a03f43e2f15972455a93244a6e127ed8b4
ron <rinbar70@gmail.com>
2020-11-07 22:10:11 +0200

    Demonstrate what happens when you try to manually set the Origin header.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const dataRouter = require('./routers/data');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/data', cors({origin: 'http://localhost:4000'}), dataRouter);
 
 module.exports = app;
